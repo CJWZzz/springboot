e7ed9d4f19fd091dfab64dfa88735e00bebe07ee
Zzz <yjzzz45@gmail.com>
2020-03-14 18:05:17 +0800

    spingboot learen

---
+++ src/main/java/com/example/springboot/com/example/springboot/web/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.springboot.com.example.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+@Controller
+public class HelloController {
+
+    //根据@RequestMapping设置的字符串来访问方法
+    @RequestMapping("/hello")
+    public String hello(Model m) throws Exception {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        if (true) {
+            throw new Exception("some exception");
+        }
+        //根据返回的字符串加上.jsp匹配jsp文件
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,10 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Zzz
+  Date: 2020/3/14
+  Time: 15:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+Hi JSP. 现在时间是  ${now}
